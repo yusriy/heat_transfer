2e6c7be35eb7057ba4fc865a5c2cbbe736a9131b
yusriy <yusriyp@gmail.com>
2015-09-06 12:07:17 +0800

    Made the figures bigger.

--- R/plot_results.R
+++ R/plot_results.R
@@ -7,7 +7,7 @@ time_step <- result$time_step + 1 # Always an extra step after
 
 # The plots
 for (i in 1:time_step) {
-  png(filename = paste('figs/heat',i,'.png',sep=''), height = 200, width = 400)
+  png(filename = paste('figs/heat',i,'.png',sep=''), height = 350, width = 400)
   plot(result$dist_matrix[i,],result$Temperature[i,], type = 'l',
        ylab = 'Temperature (deg C)', xlab = 'Distance (m)', 
        main = paste('t = ', result$time_matrix[i,1], ' s'),
